9bb6ac35049d3432d3b92e7846c25e051a228872
ding-co <pangoons@naver.com>
2021-12-28 04:57:58 +0900

    Add Algorithms - level1: 2 problems

---
+++ algorithms/level1/insufficient-amount.py
@@ -0,0 +1,7 @@
+def solution(price, money, count):
+    price_sum = 0
+    for i in range(1, count + 1):
+        price_sum += price * i
+    if price_sum > money:
+        return price_sum - money
+    return 0
