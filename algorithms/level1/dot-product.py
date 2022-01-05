a766b93c04c8d08a738eb4610f8a75095e81b734
ding-co <pangoons@naver.com>
2022-01-05 13:33:56 +0900

    [ADD] Algorithms - level1: 1 problem

---
+++ algorithms/level1/dot-product.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    result = 0
+    for i in range(len(a)):
+        result += (a[i] * b[i])
+    return result
+
+# Other Solution
+# def solution(a, b):
+#     return sum([x * y for x, y in zip(a, b)])
